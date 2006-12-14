1850aa92eb9841fdfa71eef2ff2b63d937ff2d57
Benja Fallenstein <benja.fallenstein@gmail.com>
2006-12-14 00:37:29 +0000

    store widths and heights in vobscenes and pass them to vobs' draw methods

--- Vobs.hs
+++ Vobs.hs
@@ -4,52 +4,58 @@ import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk.Cairo
 
 import Data.List (intersect)
-import Data.Map (Map, keys, (!), fromList, toList)
+import Data.Map (Map, keys, (!), fromList, toList, insert, empty)
 
-data Vob = Vob { vobSize :: Render (Double, Double), drawVob :: Render () }
+data Vob = Vob { defaultSize :: Render (Double, Double), 
+                 drawVob :: Double -> Double -> Render () }
 
-vobWidth v  = do (w,h) <- vobSize v; return w
-vobHeight v = do (w,h) <- vobSize v; return h
+defaultWidth v  = do (w,h) <- defaultSize v; return w
+defaultHeight v = do (w,h) <- defaultSize v; return h
 
     
 hbox :: [Vob] -> Vob
 hbox vobs = Vob size draw where
-    size = do sizes <- mapM vobSize vobs
+    size = do sizes <- mapM defaultSize vobs
               let (widths, heights) = unzip sizes
               return (sum widths, maximum heights)
 
-    draw = do save
-              sequence $ flip map vobs $ \vob -> do
-                  drawVob vob
-                  w <- vobWidth vob; translate w 0
-              restore
+    draw w h = do save
+                  sequence $ flip map vobs $ \vob -> do
+                      vobW <- defaultWidth vob
+                      drawVob vob vobW h
+                      translate vobW 0
+                  restore
     
 label :: String -> Vob
 label s = Vob (do ext <- textExtents s
                   return (textExtentsWidth ext, textExtentsHeight ext))
-              (do ext <- textExtents s
-                  save; moveTo 0 (textExtentsHeight ext); showText s; restore)
+              (\w h -> do ext <- textExtents s
+                          save; moveTo 0 (textExtentsHeight ext)
+                          showText s; restore)
                   
                   
 scaleVob :: Double -> Double -> Vob -> Vob
 scaleVob sx sy (Vob size draw) = Vob size' draw' where
-    size' = do (w,h) <- size; return (sx*w, sy*h)
-    draw' = do save; scale sx sy; draw; restore
+    size'     = do (w,h) <- size; return (sx*w, sy*h)
+    draw' w h = do save; scale sx sy; draw (sx*w) (sy*h); restore
     
     
 rectBox :: Vob -> Vob
 rectBox (Vob size draw) = Vob size' draw' where
-    size' = do (w,h) <- size; return (w+2, h+2)
-    draw' = do (w,h) <- size; save
-               rectangle 0 0 (w+1) (h+1); stroke
-               translate 1 1; draw
-               restore
+    size'     = do (w,h) <- size; return (w+2, h+2)
+    draw' w h = do save
+                   rectangle 0 0 w h; stroke
+                   translate 1 1; draw (w-2) (h-2)
+                   restore
                
                
 pad4 :: Double -> Double -> Double -> Double -> Vob -> Vob
 pad4 left up right down (Vob size draw) = Vob size' draw' where
-    size' = do (w,h) <- size; return (left+w+right, up+h+down)
-    draw' = do save; translate left up; draw; restore
+    size'     = do (w,h) <- size; return (left+w+right, up+h+down)
+    draw' w h = do save
+                   translate left up
+                   draw (w-left-right) (h-up-down)
+                   restore
     
 pad2 x y   = pad4 x y x y
 pad pixels = pad2 pixels pixels
@@ -67,16 +73,16 @@ resize w h (Vob size draw) = Vob (return (w,h)) draw
 
 clipVob :: Vob -> Vob
 clipVob (Vob size draw) = Vob size draw' where
-    draw' = do save; (w,h) <- size; rectangle 0 0 w h; clip; draw; restore
+    draw' w h = do save; rectangle 0 0 w h; clip; draw w h; restore
     
     
     
-type Scene a = Map a (Double, Double, Vob)
+type Scene a = Map a (Double, Double, Double, Double, Vob)
 
-sceneVob :: Ord a => Double -> Double -> Scene a -> Vob
-sceneVob w h scene = Vob (return (w,h)) $ do
-    flip mapM (toList scene) $ \(key, (x, y, vob)) -> do 
-        save; translate x y; drawVob vob; restore
+sceneVob :: Ord a => Scene a -> Vob
+sceneVob scene = Vob (return (0,0)) $ \_ _ -> do
+    flip mapM (toList scene) $ \(key, (x, y, w, h, vob)) -> do 
+        save; translate (x-w/2) (y-h/2); drawVob vob w h; restore
     return ()
         
         
@@ -84,20 +90,16 @@ sceneVob w h scene = Vob (return (w,h)) $ do
 interpolate :: Ord a => Double -> Scene a -> Scene a -> Scene a
 interpolate fract src dst = let
       ks = intersect (keys src) (keys dst)
-      interp a b = (a*(1-fract)) + (b*fract)
-      vob (sx,sy,sv) (dx,dy,dv) = (interp sx dx, interp sy dy, dv)
+      interp a b = (a*(1-fract)) + (b*fract)   -- interpolate two Doubles
+      vob (sx,sy,sw,sh,sv) (dx,dy,dw,dh,dv) = 
+          (interp sx dx, interp sy dy, interp sw dw, interp sh dh,dv)
     in fromList [(k, vob (src ! k) (dst ! k)) | k <- ks]
-    
-    
              
 
 
 
 myVob = rectBox $ pad 5 $
     scaleVob 1 1 (hbox [label "Hello W", label "orld!"])
-    
-myScene1 = fromList [("Foo", (50, 50, myVob))]
-myScene2 = fromList [("Foo", (150, 150, myVob))]
 
 
 main = do
@@ -116,12 +118,17 @@ main = do
         renderWithDrawable drawable $ do
             save
             
+            (vw, vh) <- defaultSize myVob
+            
+            let myScene1 = fromList [("Foo", (50, 50, vw, vh, myVob))]
+                myScene2 = fromList [("Foo", (150, 150, vw+30, vh, myVob))]
+            
             setSourceRGB 0.5 0.5 0.5
-            drawVob (sceneVob w h myScene1)
-            drawVob (sceneVob w h myScene2)
+            drawVob (sceneVob myScene1) w h
+            drawVob (sceneVob myScene2) w h
             
             setSourceRGB 0 0 0
-            drawVob (sceneVob w h (interpolate 0.5 myScene1 myScene2))
+            drawVob (sceneVob $ interpolate 0.5 myScene1 myScene2) w h
             
             restore
             
