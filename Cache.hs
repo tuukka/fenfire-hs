2aa4f5aac50b60e423316f89c569b4ecc8904882
Benja Fallenstein <benja.fallenstein@gmail.com>
2007-02-08 12:44:40 +0000

    use weak values in cache

--- Cache.hs
+++ Cache.hs
@@ -10,6 +10,7 @@ import Data.Unique
 
 import System.IO.Unsafe
 import System.Mem.StableName
+import System.Mem.Weak
 
 
 class Hashable a where
@@ -35,14 +36,16 @@ byAddress :: a -> StableName a
 byAddress = unsafePerformIO . makeStableName
 
 
-type Cache key value = HashTable key value
+type Cache key value = HashTable key (Weak value)
 
 newCache :: (Eq key, Hashable key) => Cache key value
 newCache = unsafePerformIO $ HashTable.new (==) hash
 
 cached :: (Eq k, Hashable k) => k -> Cache k v -> v -> v
 cached key cache val = unsafePerformIO $ do
-    val' <- HashTable.lookup cache key
+    wval' <- HashTable.lookup cache key
+    val' <- if isJust wval' then deRefWeak $ fromJust wval' else return Nothing
     if isJust val' then return $ fromJust val' else do
-        HashTable.insert cache key val
+        wval <- mkWeakPtr val $ Just $ HashTable.delete cache key
+        HashTable.insert cache key wval
         return val
